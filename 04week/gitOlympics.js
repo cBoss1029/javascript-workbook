e3950c2d53c13cc0bd811c347856f157771c894c
Cory <cbosselmann1029@gmail.com>
2018-02-06 19:19:31 -0600

    added olympian names

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,6 +1,6 @@
 'use strict'
 
-  let names = ['name1', 'name2', 'name3']
+  let names = ['Michael Phelps', 'Mark Spitz', 'Carl Lewis', 'Usain Bolt', 'David Douillet']
 
 const printListOfOlympians = (arr) =>{
 arr.forEach((name)=>{
