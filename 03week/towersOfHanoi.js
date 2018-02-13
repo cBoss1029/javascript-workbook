2ab606bf87cefaa4b87cb5a4af1984b5d1ec0064
Cory <cbosselmann1029@gmail.com>
2018-02-13 03:13:16 -0600

    es6 syntax, added reset function, game works now

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,57 +13,62 @@ let stacks = {
   c: []
 };
 
-function printStacks() {
+const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
-
-function movePiece(startStack, endStack) {
+const resetStacks = () => {
+  stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+  };
+}
+const movePiece = (startStack, endStack) => {
   // Your code here
   const pieceToMove = stacks[startStack].pop();
-  // console.log(pieceToMove);
   stacks[endStack].push(pieceToMove);
 
 }
 
-function isLegal(startStack, endStack) {
+const isLegal = (startStack, endStack) => {
   // Your code here
-  console.log('test');
-  console.log(stacks[startStack][stacks[startStack].length-1])
-  // console.log(stacks[endStack].length);
-  // console.log(stacks[startStack].length);
-  // const secondStack = stacks[endStack].length;
-  // console.log(secondStack);
-  if (stacks[endStack].length === 0){
+  if (stacks[endStack].length === 0) {
     return true;
-  } else if (stacks[startStack][stacks[startStack].length-1] >
-    stacks[endStack][stacks[endStack].length-1]) {
-      console.log('false dude');
+  } else if (stacks[startStack][stacks[startStack].length - 1] >
+    stacks[endStack][stacks[endStack].length - 1]) {
     return false;
-  } else if (stacks[startStack][stacks[startStack].length-1] <
-  stacks[endStack][stacks[endStack].length-1]){
-    console.log('true dude');
+  } else if (stacks[startStack][stacks[startStack].length - 1] <
+    stacks[endStack][stacks[endStack].length - 1]) {
     return true;
-  } else if (stacks[startStack].length === 0){
-    console.log('nothing to move');
+  } else if (stacks[startStack].length === 0) {
     return false;
   }
 }
 
-function checkForWin() {
+const checkForWin = () => {
   // Your code here
-  
+  if (stacks.b.length === 4 || stacks.c.length === 4) {
+    resetStacks();
+    return 'You win!';
+
+
+  }
 
 }
 
-function towersOfHanoi(startStack, endStack) {
+const towersOfHanoi = (startStack, endStack) => {
   // Your code here
-  isLegal(startStack, endStack);
-  movePiece(startStack,endStack)
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack);
+    checkForWin();
+  } else {
+    return 'Invalid input';
+  }
 }
 
-function getPrompt() {
+const getPrompt = () => {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
