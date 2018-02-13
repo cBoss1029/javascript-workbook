667b97e8713a836cb88b299fe88a2195584f0274
Cory <cbosselmann1029@gmail.com>
2018-02-13 02:10:39 -0600

    towers initial commit

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,24 +19,48 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
   // Your code here
+  const pieceToMove = stacks[startStack].pop();
+  // console.log(pieceToMove);
+  stacks[endStack].push(pieceToMove);
 
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
   // Your code here
-
+  console.log('test');
+  console.log(stacks[startStack][stacks[startStack].length-1])
+  // console.log(stacks[endStack].length);
+  // console.log(stacks[startStack].length);
+  // const secondStack = stacks[endStack].length;
+  // console.log(secondStack);
+  if (stacks[endStack].length === 0){
+    return true;
+  } else if (stacks[startStack][stacks[startStack].length-1] >
+    stacks[endStack][stacks[endStack].length-1]) {
+      console.log('false dude');
+    return false;
+  } else if (stacks[startStack][stacks[startStack].length-1] <
+  stacks[endStack][stacks[endStack].length-1]){
+    console.log('true dude');
+    return true;
+  } else if (stacks[startStack].length === 0){
+    console.log('nothing to move');
+    return false;
+  }
 }
 
 function checkForWin() {
   // Your code here
+  
 
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+  isLegal(startStack, endStack);
+  movePiece(startStack,endStack)
 }
 
 function getPrompt() {
@@ -50,5 +74,3 @@ function getPrompt() {
 }
 
 getPrompt();
-
-
