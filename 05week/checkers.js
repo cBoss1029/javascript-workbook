b0083ee25c19a82f2a41e738277b9dd4f95f5d35
Cory <cbosselmann1029@gmail.com>
2018-02-28 20:00:05 -0600

    FINALLY passing all tests, just needs checkForWin function

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -21,7 +21,8 @@ class Board {
   constructor() {
 
     this.grid = [];
-    this.checkers = 24;
+    this.checkers = [];
+
     // creates an 8x8 array, filled with null values
 
   }
@@ -46,6 +47,7 @@ class Board {
   viewGrid() {
     // add our column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
+
     for (let row = 0; row < 8; row++) {
       // we start with our row number in our array
       const rowOfCheckers = [row];
@@ -54,47 +56,86 @@ class Board {
         // if the location is "truthy" (contains a checker piece, in this case)
         if (this.grid[row][column]) {
           // push the symbol of the check in that location into the array
+
           rowOfCheckers.push(this.grid[row][column]);
+          // checkers.push(this.grid[row][column]);
+
+        // } else if (this.grid.indexOf(this.grid[row]) > 4 && this.grid[row][column]) {
+        //   rowOfCheckers.push(this.grid[row][column].symbol2);
+          // checkers.push(this.grid[row][column]);
+
+
         } else {
           // just push in a blank space
           rowOfCheckers.push(' ');
         }
       }
+
       // join the rowOfCheckers array to a string, separated by a space
       string += rowOfCheckers.join(' ');
       // add a 'new line'
       string += "\n";
     }
     console.log(string);
+    console.log(this.checkers.length)
   }
+
 
   playerStartingPositions() {
     const checker = new Checker();
-    const p1 = checker.symbol2;
-    const p2 = checker.symbol1;
-    this.grid.forEach((row, index) => {
-      if (index % 2 === 0 && index < 3) {
-        this.grid[index] = [null, p1, null, p1, null, p1, null, p1];
-      } else if (index > 2 && index < 5) {
-        this.grid[index] = [null, null, null, null, null, null, null, null];
-      } else if (index % 2 === 0 && index > 4) {
-        this.grid[index] = [null, p2, null, p2, null, p2, null, p2];
-      } else if (index % 2 !== 0 && index < 3) {
-        this.grid[index] = [p1, null, p1, null, p1, null, p1, null];
-      } else if (index % 2 !== 0 && index > 4) {
-        this.grid[index] = [p2, null, p2, null, p2, null, p2, null];
-      }
-    })
-    console.log(this.grid);
-  }
+    // const p1 = checker;
+    // const p2 = checker.symbol1;
+    // this.grid.forEach((row, index) => {
+    //   if (index % 2 === 0 && index < 3) {
+    //     this.grid[index] = [null, checker, null, checker, null, checker, null, checker];
+    //   } else if (index % 2 !== 0 && index < 3) {
+    //     this.grid[index] = [checker, null, checker, null, checker, null, checker, null];
+    //   } else if (index > 2 && index < 5) {
+    //     this.grid[index] = [null, null, null, null, null, null, null, null];
+    //   } else if (index % 2 === 0 && index > 4) {
+    //     this.grid[index] = [null, checker, null, checker, null, checker, null, checker];
+    //   } else if (index % 2 !== 0 && index > 4) {
+    //     this.grid[index] = [checker, null, checker, null, checker, null, checker, null];
+    //   }
+    // })
 
+    this.grid.forEach((row, index1) => {
+      row.forEach((column, index2) => {
+        if (index1 % 2 === 0 && index1 < 3) {
+          if (index2 % 2 !== 0) {
+            this.grid[index1][index2] = checker.symbol1;
+            this.checkers.push(this.grid[index1][index2]);
+          }
+        } else if (index1 % 2 !== 0 && index1 < 3) {
+          if (index2 % 2 === 0) {
+            this.grid[index1][index2] = checker.symbol1;
+            this.checkers.push(this.grid[index1][index2])
+          }
+        } else if (index1 % 2 === 0 && index1 > 4) {
+          if (index2 % 2 !== 0) {
+            this.grid[index1][index2] = checker.symbol2;
+            this.checkers.push(this.grid[index1][index2])
+          }
+        } else if (index1 % 2 !== 0 && index1 > 4) {
+          if (index2 % 2 === 0) {
+            this.grid[index1][index2] = checker.symbol2;
+            this.checkers.push(this.grid[index1][index2])
+          }
+        }
+      })
+
+      // console.log(this.grid);
+      // console.log(this.checkers);
+    })
+
+  }
 }
 
 class Game {
   constructor() {
 
     this.board = new Board();
-    this.playerTurn = 'A';
+    this.playerTurn = 'B';
     this.player1ClaimedPieces = 0;
     this.player2ClaimedPieces = 0;
   }
@@ -105,76 +146,77 @@ class Game {
     this.board.playerStartingPositions();
 
   }
-  stringToNumber(str) {
-    str = str.split('');
-    str.forEach((item) => {
-      parseInt(item);
-    })
-    return str;
-  }
+
   moveChecker(whichPiece, toWhere) {
     whichPiece = whichPiece.split('').map(Number);
     toWhere = toWhere.split('').map(Number);
     if (this.isLegalMove(whichPiece, toWhere) && this.jumpedPiece(whichPiece, toWhere)) {
       if (this.playerTurn === 'A') {
         if (whichPiece[1] < toWhere[1]) {
-          this.board.grid[toWhere[0]-1][toWhere[1]-1] = null;
+          this.board.grid[toWhere[0] - 1][toWhere[1] - 1] = null;
         } else if (whichPiece[1] > toWhere[1]) {
-          this.board.grid[toWhere[0]-1][toWhere[1]+1] = null;
+          this.board.grid[toWhere[0] - 1][toWhere[1] + 1] = null;
         }
+        this.board.grid[toWhere[0]][toWhere[1]] = this.board.grid[whichPiece[0]][whichPiece[1]];
         this.board.grid[whichPiece[0]][whichPiece[1]] = null;
-        this.board.grid[toWhere[0]][toWhere[1]] = this.playerTurn;
-        this.player1ClaimedPieces ++;
+        this.player1ClaimedPieces++;
+        this.board.checkers.shift();
         this.playerTurn = 'B'
         console.log(this.board.grid);
         // console.log(game.board.checkers.length);
         console.log(this.player1ClaimedPieces);
+        console.log("checkers :", this.board.checkers.length);
 
       } else {
         if (whichPiece[1] < toWhere[1]) {
-          this.board.grid[toWhere[0]+1][toWhere[1]-1] = null;
+          this.board.grid[toWhere[0] + 1][toWhere[1] - 1] = null;
         } else if (whichPiece[1] > toWhere[1]) {
-          this.board.grid[toWhere[0]+1][toWhere[1]+1] = null;
+          this.board.grid[toWhere[0] + 1][toWhere[1] + 1] = null;
         }
+        this.board.grid[toWhere[0]][toWhere[1]] = this.board.grid[whichPiece[0]][whichPiece[1]];
         this.board.grid[whichPiece[0]][whichPiece[1]] = null;
-        this.board.grid[toWhere[0]][toWhere[1]] = this.playerTurn;
-        this.player2ClaimedPieces ++;
+        this.player2ClaimedPieces++;
+        this.board.checkers.shift();
         this.playerTurn = 'A'
         console.log(this.board.grid);
         console.log(this.player2ClaimedPieces);
+        console.log('checkers:', this.board.checkers.length);
+
+
 
 
       }
     } else if (this.isLegalMove(whichPiece, toWhere)) {
       if (this.playerTurn === 'A') {
+        this.board.grid[toWhere[0]][toWhere[1]] = this.board.grid[whichPiece[0]][whichPiece[1]];
         this.board.grid[whichPiece[0]][whichPiece[1]] = null;
-        this.board.grid[toWhere[0]][toWhere[1]] = this.playerTurn;
         this.playerTurn = 'B'
         console.log(this.board.grid);
         console.log(game.board.checkers.length);
 
       } else {
+        this.board.grid[toWhere[0]][toWhere[1]] = this.board.grid[whichPiece[0]][whichPiece[1]];
         this.board.grid[whichPiece[0]][whichPiece[1]] = null;
-        this.board.grid[toWhere[0]][toWhere[1]] = this.playerTurn;
         this.playerTurn = 'A'
         console.log(this.board.grid);
+
 
       }
     }
 
   }
   jumpedPiece(whichPiece, toWhere) {
-    if(this.playerTurn === 'A' &&
+    if (this.playerTurn === 'A' &&
       toWhere[0] - whichPiece[0] === 2 &&
       !this.board.grid[toWhere[0]][toWhere[1]]) {
       return true;
     } else if (this.playerTurn === 'B' &&
       whichPiece[0] - toWhere[0] === 2 &&
       !this.board.grid[toWhere[0]][toWhere[1]]) {
-        return true;
-      } else {
-        return false;
-      }
+      return true;
+    } else {
+      return false;
+    }
   }
   isLegalMove(whichPiece, toWhere) {
     if (this.playerTurn === 'A' &&
